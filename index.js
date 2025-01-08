89caf8f7beb9b136c54d156dc01689f2f56754f3
Sadmaan warshi <spwarshi@gmail.com>
2025-01-09 00:12:08 +0530

    public folder updated

--- index.js
+++ index.js
@@ -33,7 +33,8 @@ const port = process.env.PORT || 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
